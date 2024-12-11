6f5673f1cfb6910d7bb66f5d8db87aed1fb20c72
Madan Mohan Venkatesan <91960304+madanmohan-venkatesan@users.noreply.github.com>
2024-12-11 05:43:42 +0000

    new file:   .vscode/tasks.json 	new file:   structures 	new file:   structures.cpp

---
+++ structures.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct rectangle
+{
+    int lenght;
+    int breadth;
+};
+int main()
+{
+    rectangle a;
+    rectangle *p;
+    p=(rectangle *)malloc(sizeof(rectangle));
+    p->lenght=55;
+    p->breadth=50;
+    a.lenght=10;
+    a.breadth=20;
+    cout << "Size using Pointers\n"<< p->lenght << endl << p->breadth << endl;
+    cout << "Size using parameters\n"<< a.lenght << endl << a.breadth << endl;
+    return 0;
+
+}
