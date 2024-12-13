1abfcda7e186598a249bcbbf122124d5f56c2c63
Madanmohan Venkatesan <madanrokz@gmail.com>
2024-12-13 12:44:45 +0530

    Second commit

--- structures.cpp
+++ structures.cpp
@@ -17,5 +17,4 @@ int main()
     cout << "Size using Pointers\n"<< p->lenght << endl << p->breadth << endl;
     cout << "Size using parameters\n"<< a.lenght << endl << a.breadth << endl;
     return 0;
-
 }
